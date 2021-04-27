136a760f635386635bfff29cbf4b4b0b2d9a019f
Kevin Ramos <61996702+KevuRamos@users.noreply.github.com>
2021-04-26 23:19:27 -0300

    Criação da atividade

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Cliente } from './clientes';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'form-tutorial';
+  cliente: Cliente = new Cliente();
+  constructor(private http: HttpClient) {
+  }
+  VerificaCEP() {
+  this.http.get(`https://viacep.com.br/ws/${this.cliente.CEP}/json/`).toPromise().then(
+  data => {
+  const dado: any = data;
+  this.cliente.Endereco = dado.logradouro;
+  }
+  );
+  }
+  salvar() {
+    console.log(this.cliente);
+    }
+  }
